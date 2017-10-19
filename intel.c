fb8896a0fe07c2e0255f4e3b5c583ce3136036d1
Zibri <Zibri@users.noreply.github.com>
2017-10-19 19:49:35 +0200

    Uploaded C sources and FI scripts.

---
+++ intel.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int fd1=-1;
+    unsigned char data=0x4c;
+    unsigned char data2=0x00;
+
+    fd1=open("/dev/port",O_RDWR|O_NDELAY);
+    if ( fd1 < 0 ) return fd1;
+
+    lseek(fd1,(0x70),SEEK_SET);
+    write(fd1,&data,1);
+
+    lseek(fd1,(0x71),SEEK_SET);
+    write(fd1,&data2,1);
+
+
+    return 0;
+}
+
